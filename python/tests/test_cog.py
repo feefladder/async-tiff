5daebd98f50831638876b215df3a9ea00436f980
Wei Ji <23487320+weiji14@users.noreply.github.com>
2025-05-07 08:08:23 +1200

    Enable pytest-asyncio tests in CI (#92)
    * Set asyncio_mode="auto" in pytest.ini config
    
    Xref https://pytest-asyncio.readthedocs.io/en/latest/reference/configuration.html#asyncio-mode
    
    * Refactor contents of test_cog.py into a proper unit test
    
    Put everything in an `async def test_cog_s3` function, with assertions checking different metadata attributes.
    
    * Uncomment `uv run pytest` in .github/workflows/test-python.yml
    
    * Add `--color=yes`flag to force GitHub Actions logs to have color
    
    Xref https://github.com/pytest-dev/pytest/issues/7443
    
    * Ruff format
    
    * Remove timing assertions
    
    * Bump astral-sh/setup-uv from 3 to 6
    
    Bumps [astral-sh/setup-uv](https://github.com/astral-sh/setup-uv) from 3 to 6.
    - [Release notes](https://github.com/astral-sh/setup-uv/releases)
    - [Commits](https://github.com/astral-sh/setup-uv/compare/v3...v6)
    
    * Remove `uv sync` part
    
    * Bump pytest-asyncio from 0.24.0 to 0.26.0
    
    Bumps [pytest-asyncio](https://github.com/pytest-dev/pytest-asyncio) from 0.24.0 to 0.26.0.
    - [Release notes](https://github.com/pytest-dev/pytest-asyncio/releases)
    - [Commits](https://github.com/pytest-dev/pytest-asyncio/compare/v0.24.0...v0.26.0)
    
    Also set asyncio_default_fixture_loop_scope="function" following https://github.com/pytest-dev/pytest-asyncio/issues/924#issuecomment-2326877541
    
    * Revert "Remove `uv sync` part"
    
    This reverts commit 638d6029b03fe15262386ceb98015514ed8579d8.
    
    * Typo geoindex-rs -> async-tiff
    
    * Add back `--no-project` flag to `uv run` commands
    
    Partially undo bfb3a43851910c446ace0a589529b50f5821657d

--- python/tests/test_cog.py
+++ python/tests/test_cog.py
@@ -1,27 +1,25 @@
-import async_tiff
-from time import time
-from async_tiff import TIFF
+from async_tiff import TIFF, enums
 from async_tiff.store import S3Store
 
-store = S3Store("sentinel-cogs", region="us-west-2", skip_signature=True)
-path = "sentinel-s2-l2a-cogs/12/S/UF/2022/6/S2B_12SUF_20220609_0_L2A/B04.tif"
 
-tiff = await TIFF.open(path, store=store, prefetch=32768)
+async def test_cog_s3():
+    """
+    Ensure that TIFF.open can open a Sentinel-2 Cloud-Optimized GeoTIFF file from an
+    s3 bucket, read IFDs and GeoKeyDirectory metadata.
+    """
+    path = "sentinel-s2-l2a-cogs/12/S/UF/2022/6/S2B_12SUF_20220609_0_L2A/B04.tif"
+    store = S3Store("sentinel-cogs", region="us-west-2", skip_signature=True)
+    tiff = await TIFF.open(path=path, store=store, prefetch=32768)
 
-start = time()
-tiff = await TIFF.open(path, store=store, prefetch=32768)
-end = time()
-end - start
+    ifds = tiff.ifds
+    assert len(ifds) == 5
 
-ifds = tiff.ifds
-ifd = ifds[0]
-ifd.compression
-ifd.tile_height
-ifd.tile_width
-ifd.photometric_interpretation
-gkd = ifd.geo_key_directory
-gkd.citation
-gkd.projected_type
-gkd.citation
+    ifd = ifds[0]
+    assert ifd.compression == enums.CompressionMethod.Deflate
+    assert ifd.tile_height == 1024
+    assert ifd.tile_width == 1024
+    assert ifd.photometric_interpretation == enums.PhotometricInterpretation.BlackIsZero
 
-dir(gkd)
+    gkd = ifd.geo_key_directory
+    assert gkd.citation == "WGS 84 / UTM zone 12N"
+    assert gkd.projected_type == 32612
